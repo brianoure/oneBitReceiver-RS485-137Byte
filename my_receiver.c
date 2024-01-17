29e315902aec0f6dc850516789b3a289c4acf169
brianoure <61065227+brianoure@users.noreply.github.com>
2024-01-17 21:58:02 +0300

    Update my_receiver.c

--- my_receiver.c
+++ my_receiver.c
@@ -31,49 +31,49 @@ int communication_integer_list[6 ];
 int postamble_integer_list    [15];
 
 char integer_to_character(integer){
-    if(integer==0 ){return "a";}
-    if(integer==1 ){return "b";}
-    if(integer==2 ){return "c";}
-    if(integer==3 ){return "d";}
-    if(integer==4 ){return "e";}
-    if(integer==5 ){return "f";}
-    if(integer==6 ){return "g";}
-    if(integer==7 ){return "h";}
-    if(integer==8 ){return "i";}
-    if(integer==9 ){return "j";}
-    if(integer==10){return "k";}
-    if(integer==11){return "l";}
-    if(integer==12){return "m";}
-    if(integer==13){return "n";}
-    if(integer==14){return "o";}
-    if(integer==15){return "p";}
-    if(integer==16){return "q";}
-    if(integer==17){return "r";}
-    if(integer==18){return "s";}
-    if(integer==19){return "t";}
-    if(integer==20){return "u";}
-    if(integer==21){return "v";}
-    if(integer==22){return "w";}
-    if(integer==23){return "x";}
-    if(integer==24){return "y";}
-    if(integer==25){return "z";}
-    if(integer==26){return "0";}
-    if(integer==27){return "1";}
-    if(integer==28){return "2";}
-    if(integer==29){return "3";}
-    if(integer==30){return "4";}
-    if(integer==31){return "5";}
-    if(integer==32){return "6";}
-    if(integer==33){return "7";}
-    if(integer==34){return "8";}
-    if(integer==35){return "9";}
-    if(integer==36){return ".";}
-    if(integer==37){return "%";}
-    if(integer==38){return ";";}
-    if(integer==39){return "#";}
-    if(integer==40){return "+";}
-    if(integer==41){return "-";}
-    return "!";
+if(integer==0 ){return "a";}
+if(integer==1 ){return "b";}
+if(integer==2 ){return "c";}
+if(integer==3 ){return "d";}
+if(integer==4 ){return "e";}
+if(integer==5 ){return "f";}
+if(integer==6 ){return "g";}
+if(integer==7 ){return "h";}
+if(integer==8 ){return "i";}
+if(integer==9 ){return "j";}
+if(integer==10){return "k";}
+if(integer==11){return "l";}
+if(integer==12){return "m";}
+if(integer==13){return "n";}
+if(integer==14){return "o";}
+if(integer==15){return "p";}
+if(integer==16){return "q";}
+if(integer==17){return "r";}
+if(integer==18){return "s";}
+if(integer==19){return "t";}
+if(integer==20){return "u";}
+if(integer==21){return "v";}
+if(integer==22){return "w";}
+if(integer==23){return "x";}
+if(integer==24){return "y";}
+if(integer==25){return "z";}
+if(integer==26){return "0";}
+if(integer==27){return "1";}
+if(integer==28){return "2";}
+if(integer==29){return "3";}
+if(integer==30){return "4";}
+if(integer==31){return "5";}
+if(integer==32){return "6";}
+if(integer==33){return "7";}
+if(integer==34){return "8";}
+if(integer==35){return "9";}
+if(integer==36){return ".";}
+if(integer==37){return "%";}
+if(integer==38){return ";";}
+if(integer==39){return "#";}
+if(integer==40){return "+";}
+if(integer==41){return "-";}
+return "!";
 }//integer_to_character
         
 void update_raw_binary_list(recent_binary){
@@ -138,68 +138,61 @@ if(value_index!=0){ start=start+8;end=end+8;integer_value_list[value_index]=inte
 return( integer_value_list );
 }/*get_8_bit_values_from_list*/
 
-def decrypt(mylist,mylistsize):
-    decrypt_result="!"*mylistsize
-    for index in range(mylistsize):
-        decrypt_result[index]=integer_to_character(mylist[index])
-    return decrypt_result
+char decrypt(mylist,mylistsize){
+char decrypt_result[mylistsize];
+for(int index=0;index<=mylistsize;index++){decrypt_result[index]=integer_to_character(mylist[index]);}
+return decrypt_result;
+}/*decrypt*/
 
-def update_binary_frame_sections():
-    update_preamble_binary_list_from_raw_list     ()
-    update_id_binary_list_from_raw_list           ()
-    update_time_binary_list_from_raw_list         ()
-    update_computer_binary_list_from_raw_list     ()
-    update_power_binary_list_from_raw_list        ()
-    update_structure_binary_list_from_raw_list    ()
-    update_attitude_binary_list_from_raw_list     ()
-    update_payload_binary_list_from_raw_list      ()
-    update_communication_binary_list_from_raw_list()
-    update_postamble_binary_list_from_raw_list    ()
+void update_binary_frame_sections(){
+update_preamble_binary_list_from_raw_list     ();
+update_id_binary_list_from_raw_list           ();
+update_time_binary_list_from_raw_list         ();
+update_computer_binary_list_from_raw_list     ();
+update_power_binary_list_from_raw_list        ();
+update_structure_binary_list_from_raw_list    ();
+update_attitude_binary_list_from_raw_list     ();
+update_payload_binary_list_from_raw_list      ();
+update_communication_binary_list_from_raw_list();
+update_postamble_binary_list_from_raw_list    ();
+}/*update_binary_frame_sections*/
 
-def get_section_messages_from_binary_lists():
-    decrypt(get_8_bit_values_from_list(preamble_binary_list     ,120),15)
-    decrypt(get_8_bit_values_from_list(id_binary_list           ,120),15)
-    decrypt(get_8_bit_values_from_list(time_binary_list         ,80 ),10)
-    decrypt(get_8_bit_values_from_list(computer_binary_list     ,80 ),10)
-    decrypt(get_8_bit_values_from_list(power_binary_list        ,160),20)
-    decrypt(get_8_bit_values_from_list(structure_binary_list    ,128),16)
-    decrypt(get_8_bit_values_from_list(attitude_binary_list     ,80 ),10)
-    decrypt(get_8_bit_values_from_list(payload_binary_list      ,160),20)
-    decrypt(get_8_bit_values_from_list(communication_binary_list,48 ),6 )
-    decrypt(get_8_bit_values_from_list(postamble_binary_list    ,120),15)
+void get_section_messages_from_binary_lists(){
+decrypt(get_8_bit_values_from_list(preamble_binary_list     ,120),15);
+decrypt(get_8_bit_values_from_list(id_binary_list           ,120),15);
+decrypt(get_8_bit_values_from_list(time_binary_list         ,80 ),10);
+decrypt(get_8_bit_values_from_list(computer_binary_list     ,80 ),10);
+decrypt(get_8_bit_values_from_list(power_binary_list        ,160),20);
+decrypt(get_8_bit_values_from_list(structure_binary_list    ,128),16);
+decrypt(get_8_bit_values_from_list(attitude_binary_list     ,80 ),10);
+decrypt(get_8_bit_values_from_list(payload_binary_list      ,160),20);
+decrypt(get_8_bit_values_from_list(communication_binary_list,48 ),6 );
+decrypt(get_8_bit_values_from_list(postamble_binary_list    ,120),15);
+}/*get_section_messages_from_binary_lists*/
 
-def run_receiver_program():
-    result=True#external control for running the receiver program
-    return result
-    
-def get_rs485_ch_one_line_A():
-    result=1#external control for retrieving bit value of channel one line A
-    return result
+void run_receiver_program(){
+result=true;/******external control for running the receiver program*********/
+return result;
+}/*run_receiver_program*/
 
-def get_rs485_ch_one_line_B():
-    result=1#external control for retrieving bit value of channel one line B
-    return result
+void get_rs485_ch_one_line_A(){
+result=1;/***external control for retrieving bit value of channel one line A***/
+return result;
+}/*get_rs485_ch_one_line_A*/
 
-def receiver_main():
-    while run_receiver_program():
-        a=get_rs485_ch_one_line_A()
-        b=get_rs485_ch_one_line_B()
-        #print(a,b)
-        if(a==0 and b==0):
-            #print("first")
-            #update_raw_binary_list(recent_binary)
-            while(get_rs485_ch_one_line_A()==0 and get_rs485_ch_one_line_B()==0):pass
-        if(a==0 and b==1):
-            #print("second")
-            update_raw_binary_list(0)
-            update_binary_frame_sections()
-            while(get_rs485_ch_one_line_A()==0 and get_rs485_ch_one_line_B()==1):pass
-        if(a==1 and b==0):
-            #print("third")
-            update_raw_binary_list(1)
-            update_binary_frame_sections()
-            while(get_rs485_ch_one_line_A()==1 and get_rs485_ch_one_line_B()==0):pass
-        if(a==1 and b==1):
-            #print("fourth")
-            #update_raw_binary_list(recent_binary)
-            while(get_rs485_ch_one_line_A()==1 and get_rs485_ch_one_line_B()==1):pass
+void get_rs485_ch_one_line_B(){
+result=1;/***external control for retrieving bit value of channel one line B***/
+return result;
+}/*get_rs485_ch_one_line_B*/:  
+
+int receiver_main():
+while(run_receiver_program()){
+int a=get_rs485_ch_one_line_A();
+int b=get_rs485_ch_one_line_B();
+if(a==0 and b==0){while(get_rs485_ch_one_line_A()==0 && get_rs485_ch_one_line_B()==0){}}
+if(a==0 and b==1){update_raw_binary_list(0);update_binary_frame_sections();while(get_rs485_ch_one_line_A()==0 && get_rs485_ch_one_line_B()==1){}}     
+if(a==1 and b==0){update_raw_binary_list(1);update_binary_frame_sections();while(get_rs485_ch_one_line_A()==1 && get_rs485_ch_one_line_B()==0){}}
+if(a==1 and b==1){while(get_rs485_ch_one_line_A()==1 && get_rs485_ch_one_line_B()==1){}}
+}
+return 0;
+}/*receiver_main*/
